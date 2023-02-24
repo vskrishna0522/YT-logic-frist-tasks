f1ce339ade8f2843414ee2bd5b89a0d443304427
vskrishna0522 <120077783+vskrishna0522@users.noreply.github.com>
2023-02-24 13:03:19 +0530

    Add files via upload

---
+++ exercise3.js
@@ -0,0 +1,8 @@
+let name = 'kittu'
+console.log(name.charCodeAt(1))
+console.log(name.indexOf('u'))
+// unique code of character and index//
+
+let myname = 'krishna'
+console.log(myname.charCodeAt(3))
+console.log(myname.indexOf('r'))
